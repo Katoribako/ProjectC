d1f276ffbab1a5ddb9c43067ecf5f60c935f5ff2
Katoribako <64684310+Katoribako@users.noreply.github.com>
2023-02-24 00:15:57 +0300

    Added ShowArray method

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -9,3 +9,12 @@
     }
     return Arr;
 }
+
+void ShowArray(string[] array)
+{
+    Console.Write(" In this array [ ");
+    for (int i = 0; i < array.Length - 1; i++)
+        Console.Write(array[i] + " | ");
+    Console.Write($"{array[array.Length - 1]} ]");
+    Console.WriteLine();
+}
